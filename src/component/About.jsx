1d0a658d7a889f439e0e188d8a846b1d1ef85a68
pratham meena <yashmeena274@gmail.com>
2024-05-26 12:22:49 +0530

    learning react02

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div>About</div>
+      <Link className='mx-10' to={`/about/1`}>01</Link>
+      <Link className='mx-10' to={`/about/2`}>02</Link>
+      <Link className='mx-10' to={`/about/3`}>03</Link>
+      <hr />
+      <Outlet />
+    </div>
+
   )
 }
 
--- src/component/MainRoutes.jsx
+++ src/component/MainRoutes.jsx
@@ -3,12 +3,17 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Home.jsx'
 import About from './About.jsx'
 import PageNotFound from './PageNotFound.jsx'
+import Details from './Details.jsx'
+
 
 const MainRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
+        <Route path='/about' element={<About/>}>
+          <Route path='/about/:id' element={<Details/>} />
+        </Route>
+        
         <Route path='*' element={<PageNotFound/>} />
       </Routes>
   )
